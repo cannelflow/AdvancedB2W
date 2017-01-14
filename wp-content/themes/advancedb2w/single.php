69ee61e9852ebf44d1e336bda5940b30b866fcb4
cannelflow <mightyrocket@hotmail.com>
2017-01-14 18:28:38 +0000

    Lesson 10

--- wp-content/themes/advancedb2w/single.php
+++ wp-content/themes/advancedb2w/single.php
@@ -3,13 +3,14 @@
     <!-- Page Header -->
     <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
     <!-- Set your background image for this header on the line below. -->
-    <header class="intro-header" style="background-image: url('img/post-bg.jpg')">
+    <?php $image1 = get_field('single_sub_image'); ?>
+    <header class="intro-header" style="background-image: url('<?php echo $image1['url']; ?>')">
         <div class="container">
             <div class="row">
                 <div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
                     <div class="post-heading">
                         <h1><?php the_title() ; ?></h1>
-                        <h2 class="subheading">Problems look mighty small from 150 miles up</h2>
+                        <h2 class="subheading"><?php the_field('single_sub_heading'); ?></h2>
                         <span class="meta">Posted by <?php the_author_posts_link(); ?> on <?php the_time('F j, Y'); ?></span>
                     </div>
                 </div>
@@ -22,7 +23,7 @@
         <div class="container">
             <div class="row">
                 <div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
-                    <?php the_content() ?>
+                    <?php the_content() ; ?>
                 </div>
             </div>
         </div>
