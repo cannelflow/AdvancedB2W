d88e2ae01d55683fa087599df12ce51f0f216865
cannelflow <mightyrocket@hotmail.com>
2017-01-14 18:07:36 +0000

    Lesson 8

--- wp-content/themes/advancedb2w/page.php
+++ wp-content/themes/advancedb2w/page.php
@@ -2,14 +2,15 @@
     <!-- Page Header -->
     <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
     <!-- Set your background image for this header on the line below. -->
-    <header class="intro-header" style="background-image: url('<?php echo get_template_directory_uri() ; ?>/img/about-bg.jpg')">
+    <?php $image = get_field('about_image'); ?>
+    <header class="intro-header" style="background-image: url('<?php echo $image['url']; ?>')">
         <div class="container">
             <div class="row">
                 <div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
                     <div class="page-heading">
                         <h1><?php the_title() ; ?></h1>
                         <hr class="small">
-                        <span class="subheading">This is what I do.</span>
+                        <span class="subheading"><?php the_field('about_sub_heading'); ?></span>
                     </div>
                 </div>
             </div>
